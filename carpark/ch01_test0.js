430b841994f30c70c5f64d3951cb1b7bb52ae2df
Moonhyemin <mhm130678@gmail.com>
2020-10-28 15:35:33 +0900

    commit(1028)

--- carpark/ch01_test0.js
+++ carpark/ch01_test0.js
@@ -19,3 +19,6 @@ console.log('1부터 1000까지 더한 결과물 : %d', result);
 
 var Person = {name:"소녀시대", age : 20};
 console.dir(Person);
+
+console.log('argv 속성의 파라미터 수 : '+process.argv.length);
+console.dir(process.argv);
