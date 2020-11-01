dfb9578ff595e230a8ec677d777f7fe4a43be76c
Moonhyemin <mhm130678@gmail.com>
2020-11-02 00:18:00 +0900

    commit~

--- carpark/main.js
+++ carpark/main.js
@@ -1,54 +1,34 @@
-var http = require('http');
-var fs = require('fs');
+var express = require('express')
+    , http = require('http')
+    , path = require('path');
 
-// 웹 서버 객체 생성
-var server = http.createServer();
+var bodyParser = require('body-parser')
+    , static = require('serve-static');
 
-// 웹 서버를 시작하여 3300번 포트에서 대기하도록 설정
-var port = 9000;
-server.listen(port, function(){
-    console.log('웹서버 시작 : %d', port);
+var app = express();
+
+app.set('port', process.env.PORT||3000);
+
+app.use(bodyParser.urlencoded({extend: false}));
+
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname+'/public'));
+
+app.use(function(req, res, next){
+    console.log('첫 번째 미들웨어 요청을 처리함.');
+    console.log(req);
+
+    var paramCity = req.body.id||req.query.id;
+    var paramcounty = req.body.county||req.query.county;
+
+    res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
+    res.write('<h1>입력한 정보 입니다.</h1>');
+    res.write('<div><p>paramCity : '+paramCity+'</p></div>');
+    res.write('<div><p>paramcounty : '+paramcounty+'</p></div>');
+    res.end();
 });
 
-// 클라이언트 연결 이벤트 처리
-server.on('connection', function(socket){
-    var addr = socket.address();
-    console.log('클라이언트 접속했습니다.');
-});
-
-// 클라이언트 요청 이벤트 처리 ***
-server.on('request', function(req, res){
-    console.log('클라이언트 요청이 들어왔습니다.');
-
-    var filename = 'node.html';
-    var infile = fs.createReadStream(filename, {flags:'r'});
-    var filelength = 0;
-    var curlength = 0;
-
-    fs.stat(filename, function(err,stats){
-        filelength = stats.size;
-    });
-
-    // 헤더 쓰기
-    res.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
-
-    // 파일 내용 스트림해서 읽어 본문 쓰기
-    infile.on('readable', function(){
-        var chunk;
-        while(null != (chunk = infile.read())){
-            console.log('읽어드린 데이터 크기 : %d 바이트', chunk.length);
-            curlength += chunk.length;
-            res.write(chunk, 'utf-8', function(err){
-                console.log('파일 부분 쓰기 완료 : %d, 파일 크기 : %d', curlength, filelength);
-                if(curlength >= filelength){
-                    res.end();
-                }
-            });
-        }
-    });
-});
-
-//서버 종료 이벤트 처리
-server.on('close', function(){
-    console.log('서버가 종료됩니다.');
+http.createServer(app).listen(3000, function(){
+    console.log('Express server start');
 });
